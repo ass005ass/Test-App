117704a086d8405e969d9f4a3eedc54e8ba394cb
Ilya Kiselev <ilja05as@yandex.ru>
2019-11-08 15:07:26 +0300

    I made a start screen.

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet,
+        Text,
+        TouchableOpacity,
+        ImageBackground, } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start worki nyour app</Text>
-    </View>
-  );
+
+class HomeScreen extends Component {
+
+        render() {
+
+                const { container, textButton, buttonStart, } = styles;
+
+                return (
+
+                    <ImageBackground
+                        style={container}
+                        source={require('././assets/image.png')}>
+
+                            <TouchableOpacity style={buttonStart}>
+                                    <Text style={textButton}> Start </Text>
+                            </TouchableOpacity>
+
+                    </ImageBackground>
+                );
+
+        }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const AppNavigator = createStackNavigator({
+        Home: {
+                screen: HomeScreen,
+                navigationOptions:{
+                       header: null
+                }
+        }
 });
 
+
+const styles = StyleSheet.create({
+        container: {
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+        },
+
+        buttonStart: {
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#EB5757',
+                height: 50,
+                width: '80%',
+                margin: 63,
+                borderRadius: 10,
+        },
+
+        textButton: {
+                fontFamily: 'Roboto',
+                fontSize: 20,
+                color: '#fff',
+        },
+});
+
+export default createAppContainer(AppNavigator)
 
